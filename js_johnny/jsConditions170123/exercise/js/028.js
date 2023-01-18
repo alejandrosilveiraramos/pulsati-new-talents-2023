2b3a4d16ae9b56ca1f45ba05c79af1e6ac8bce66
alejandrosilveiraramos <alejandrosilveiraramos3@gmail.com>
2023-01-18 09:47:20 -0300

    fixing repository, organizing folders by content

---
+++ js_johnny/jsConditions170123/exercise/js/028.js
@@ -0,0 +1,9 @@
+//028 Utilize um laço "for" para imprimir todas as letras de uma string chamada "palavra" no console
+
+let str = 'Palavra';
+
+for (let i = 0; i < str.length; i++) {
+    console.log(str);
+}
+
+console.log(str);
